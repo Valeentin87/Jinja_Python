3c1dff8a81f61a9c462ab5adcb37932da5f796d0
Valeentin87 <valeentin87@list.ru>
2022-11-18 19:57:17 +0300

    разобрали циклы и условия в jinja

--- main.py
+++ main.py
@@ -29,10 +29,35 @@ from jinja2 import Template, select_autoescape
 # чтобы не использовать конструкцию {{link | e}} можно воспользоваться классом escape встроенным в jinja, например:
 
 
-link = '''Отображение ссылок в HTML реализуется
- путём следующего синтаксиса <a href = http//..... /a>'''
+#link = '''Отображение ссылок в HTML реализуется
+# путём следующего синтаксиса <a href = http//..... /a>'''
 
-tm = Template('{{link}}')
-link_1 = select_autoescape(link)
-print(tm.render(link=link_1))
+#tm = Template('{{link}}')
+#link_1 = select_autoescape(link)
+#print(tm.render(link=link_1))
 
+# -----использование цикла for в шаблонах ----------
+
+fruits = ['apple', 'orange', 'strawberry']
+
+data = '''Сегодня на рынке предлагают следующие фрукты:
+        {% for i in fruits %}
+            {{i}}
+        {% endfor %}'''
+tm1 = Template('{{data}}')
+msg = tm1.render(fruits = fruits)
+print(msg)
+
+# ------Использование условия if в шаблонах -----------
+
+numbers = [2, 4, 7, 8, 9]
+data1 = '''Среди чисел {{nums}} на 2 без остатка делятся:
+            {% if nums == None %}
+                print('список пуст')
+            {% endif %}'''
+tm3 = Template('{{data1}}')
+msg1 = tm3.render(nums = numbers)
+print(msg1)
+
+
+tm3 = Template('{{data1}}')
